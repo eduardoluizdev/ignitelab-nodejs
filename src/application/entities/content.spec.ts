b8276b34df7ed16d1cc88333ee1b20c433dd4aba
Eduardo Luiz <hello@eduardoluiz.dev>
2022-12-17 22:59:16 -0300

    feat: start project

---
+++ src/application/entities/content.spec.ts
@@ -0,0 +1,16 @@
+import { Content } from './content';
+
+describe('Notification Content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('Você recebeu uma notificação de amizade.');
+    expect(content).toBeTruthy();
+  });
+
+  it('should not be able to create a notification content with less then 5 characters', () => {
+    expect(() => new Content('Test')).toThrow();
+  });
+
+  it('should not be able to create a notification content with more then 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
+});
